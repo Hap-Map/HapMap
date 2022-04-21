20c203be114de14f55e562c2c24863f68de78e00
Lauren Cavanaugh <lecavs@uw.edu>
2022-04-20 18:59:15 -0700

    Places model

---
+++ lib/api/place_model.dart
@@ -0,0 +1,13 @@
+class Place {
+  final String name;
+  final String id;
+
+  Place({required this.name, required this.id,});
+
+  factory Place.fromSearch(Map<String, dynamic> entry) {
+    return Place(
+      name : entry['description'],
+      id: entry['place_id'],
+    );
+  }
+}
