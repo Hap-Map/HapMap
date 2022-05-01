5c139fab03c4e94ea9be6f8076a7a48b138df281
jessehu312 <jessehu312@gmail.com>
2022-04-30 22:15:20 -0700

    Implemented basic layout of nav page

--- lib/pages/navigation_page.dart
+++ lib/pages/navigation_page.dart
@@ -12,26 +12,65 @@ class NavigationPage extends StatefulWidget {
 class _NavigationPageState extends State<NavigationPage> {
   @override
   Widget build(BuildContext context) {
-    return PageBackground(
+    return Scaffold(body: PageBackground(
         child: Column(
       children: [
-        Expanded(
-
-          child: Align(
-            alignment: Alignment.bottomCenter,
-            child: FittedBox(
-              fit: BoxFit.scaleDown,
-              child: TextButton(
-                child: Text('End Navigation', style: kTitleStyle,),
-                onPressed: () {
-                  Navigator.popUntil(context, ModalRoute.withName('search_page'));
-                },
-                style: kRedButtonStyle,
-              ),
+        Align(
+          alignment: Alignment.topCenter,
+          child: FittedBox(
+            fit: BoxFit.scaleDown,
+            child: TextButton(
+              child: const Text('End Navigation', style: kTitleStyle,),
+              onPressed: () {
+                Navigator.popUntil(context, ModalRoute.withName('search_page'));
+              },
+              style: kRedButtonStyle,
             ),
           ),
-        )
-      ],
-    ));
+        ),
+        const SizedBox(height: 10),
+        Container(
+          height: 300.0,
+          width: 360.0,
+          color: Colors.transparent,
+          child: Container(
+              decoration: BoxDecoration(
+                  color: const Color(0xfff7f9f7),
+                  borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.5),
+                      spreadRadius: 5,
+                      blurRadius: 7,
+                      offset: const Offset(0, 4),
+                    ),
+                  ],
+              ),
+              child:
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: const [
+                  Text("To: Mary Gates Hall",
+                  style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold, color: Colors.black),
+                  textAlign: TextAlign.left),
+                  Text("NOW: Prepare to turn right onto NE 45th St",
+                      style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600, color: Colors.black),
+                      textAlign: TextAlign.left),
+                  Text("Current Location: 8th Ave NE",
+                      style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600, color: Colors.black),
+                      textAlign: TextAlign.left),
+                ],
+              ),
+            )
+          ),
+          IconButton(
+            icon: Image.asset('images/hapticTouchButton.png'),
+            iconSize: 250,
+            onPressed: () {},
+            alignment: Alignment.bottomCenter,
+          )
+        ],
+      ))
+    );
   }
 }
