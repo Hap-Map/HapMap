f5ca50602f018f5d940d12a4e9b6922977bd202a
Lauren Cavanaugh <lecavs@uw.edu>
2022-05-03 23:58:21 -0700

    Removed snap to directions for polylines

--- lib/api/directions_api.dart
+++ lib/api/directions_api.dart
@@ -8,7 +8,6 @@ import 'package:hap_map/api/.key/maps.dart';
 
 class DirectionsAPI {
   static const String _directionsUrl = 'https://maps.googleapis.com/maps/api/directions/json?';
-  static const String _snapToRoads = 'https://roads.googleapis.com/v1/snapToRoads?';
   late Dio _dio;
 
   DirectionsAPI() {
@@ -38,45 +37,5 @@ class DirectionsAPI {
       throw Exception('Invalid Response code from API- Direction Search');
     }
   }
-
-  Future<List<LatLng>> getPolylines(Directions directions) async {
-    print(directions);
-    String path = "";
-    for (int i = 0; i < directions.totalSteps.length; i++) {
-      if (i == 0) {
-        path += directions.totalSteps[i].startLocation.latitude.toString();
-        path += ",";
-        path += directions.totalSteps[i].startLocation.longitude.toString();
-        path += "|";
-      }
-      path += directions.totalSteps[i].endLocation.latitude.toString();
-      path += ",";
-      path += directions.totalSteps[i].endLocation.longitude.toString();
-
-      if (i < directions.totalSteps.length - 1) {
-        path += "|";
-      }
-    }
-    Response response = await _dio.get(
-      _snapToRoads,
-      queryParameters: {
-        'interpolate': true,
-        'path': path,
-        'key': MAPS_API_KEY,
-      }
-    );
-
-    if (response.statusCode == 200) {
-      List<LatLng> polylines = [];
-      Map<String, dynamic> map = response.data;
-      if ((map['snappedPoints'] as List).isNotEmpty) {
-        for (var i in map['snappedPoints']) {
-          polylines.add(LatLng(i['location']['latitude'], i['location']['longitude']));
-        }
-      }
-      return polylines;
-    } else {
-      throw Exception('Invalid Response Code from API- Polyline Search');
-    }
-  }
+  
 }
