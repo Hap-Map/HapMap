1719e7c437886faf6e2fae4828930695f8e48900
Aditya Nayak <nayakadityam@gmail.com>
2022-05-05 00:28:48 -0700

    Implemented continuous location and UI updates for navigation_page. Now the UI refreshes every time with the new current location name when the user moves, and displays the correct user destination.

--- lib/pages/navigation_page.dart
+++ lib/pages/navigation_page.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:hap_map/constants.dart';
+
+import '../api/location_api.dart';
+import '../api/place_api.dart';
 
 class NavigationPage extends StatefulWidget {
   static const id = 'navigation_page';
+
   const NavigationPage({Key? key}) : super(key: key);
 
   @override
@@ -10,18 +15,40 @@ class NavigationPage extends StatefulWidget {
 }
 
 class _NavigationPageState extends State<NavigationPage> {
+  Position? _currentPosition;
+  String _currentPositionName = "Finding Current Location...";
+  String _destination = "Loading Destination...";
+
+  _NavigationPageState() {
+    LocationApi.addOnLocationUpdateListener(onLocationUpdated);
+    LocationApi.startLocationUpdates();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: PageBackground(
-        child: Column(
+    if (_currentPosition == null) {
+      List _arguments = ModalRoute.of(context)!.settings.arguments as List;
+      _currentPosition = _arguments[0];
+      _currentPositionName = _arguments[1];
+      _destination = _arguments[2].name;
+    }
+
+    return Scaffold(
+        body: PageBackground(
+            child: Column(
       children: [
         Align(
           alignment: Alignment.topCenter,
           child: FittedBox(
             fit: BoxFit.scaleDown,
             child: TextButton(
-              child: const Text('End Navigation', style: kTitleStyle,),
+              child: const Text(
+                'End Navigation',
+                style: kTitleStyle,
+              ),
               onPressed: () {
+                LocationApi.stopLocationUpdates();
+                LocationApi.removeOnLocationUpdateListener(onLocationUpdated);
                 Navigator.popUntil(context, ModalRoute.withName('search_page'));
               },
               style: kRedButtonStyle,
@@ -30,47 +57,60 @@ class _NavigationPageState extends State<NavigationPage> {
         ),
         const SizedBox(height: 10),
         Container(
-          height: 300.0,
-          width: 360.0,
-          color: Colors.transparent,
-          child: Container(
+            height: 300.0,
+            width: 360.0,
+            color: Colors.transparent,
+            child: Container(
               decoration: BoxDecoration(
-                  color: const Color(0xfff7f9f7),
-                  borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black.withOpacity(0.5),
-                      spreadRadius: 5,
-                      blurRadius: 7,
-                      offset: const Offset(0, 4),
-                    ),
-                  ],
+                color: const Color(0xfff7f9f7),
+                borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.5),
+                    spreadRadius: 5,
+                    blurRadius: 7,
+                    offset: const Offset(0, 4),
+                  ),
+                ],
               ),
-              child:
-              Column(
+              child: Column(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: const [
-                  Text("To: Mary Gates Hall",
-                  style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold, color: Colors.black),
-                  textAlign: TextAlign.left),
-                  Text("NOW: Prepare to turn right onto NE 45th St",
-                      style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600, color: Colors.black),
+                children: [
+                  Text("To: " + _destination,
+                      style: TextStyle(
+                          fontSize: 26,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black),
                       textAlign: TextAlign.left),
-                  Text("Current Location: 8th Ave NE",
-                      style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600, color: Colors.black),
+                  Text("NOW: Prepare to turn right onto NE 45th St",
+                      style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w600,
+                          color: Colors.black),
+                      textAlign: TextAlign.left),
+                  Text("Current Location: " + _currentPositionName,
+                      style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w600,
+                          color: Colors.black),
                       textAlign: TextAlign.left),
                 ],
               ),
-            )
-          ),
-          IconButton(
-            icon: Image.asset('images/hapticTouchButton.png'),
-            iconSize: 250,
-            onPressed: () {},
-            alignment: Alignment.bottomCenter,
-          )
-        ],
-      ))
-    );
+            )),
+        IconButton(
+          icon: Image.asset('images/hapticTouchButton.png'),
+          iconSize: 250,
+          onPressed: () {},
+          alignment: Alignment.bottomCenter,
+        )
+      ],
+    )));
+  }
+
+  onLocationUpdated(pos) {
+    PlaceApi.getPlace(pos).then((place) => setState(() {
+          _currentPosition = pos;
+          _currentPositionName = place.name;
+        }));
   }
 }
