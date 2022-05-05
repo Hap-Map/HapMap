87dd3ebda3e57bef222e26d5318715ec938cc801
Aditya Nayak <nayakadityam@gmail.com>
2022-05-05 01:18:29 -0700

    Implemented active loading and update of estimated time on confirm_page and sending of directions from confirm_page to navigation_page. The UI refreshes on each new data load.

--- lib/pages/confirm_page.dart
+++ lib/pages/confirm_page.dart
@@ -5,13 +5,13 @@ import 'package:hap_map/constants.dart';
 import 'package:hap_map/pages/navigation_page.dart';
 
 import '../api/directions_api.dart';
-import '../api/location_api.dart';
 import '../api/place_api.dart';
 import '../models/directions_model.dart';
 import '../models/place_model.dart';
 
 class ConfirmPage extends StatefulWidget {
   static const id = 'confirm_page';
+
   const ConfirmPage({Key? key}) : super(key: key);
 
   @override
@@ -19,91 +19,110 @@ class ConfirmPage extends StatefulWidget {
 }
 
 class _ConfirmPageState extends State<ConfirmPage> {
+  Position? _position;
   String _positionName = "Finding Current Location...";
+  Place? _destination;
+  String _destinationName = "Loading Destination...";
+  String _estimatedTime = "Loading Estimated Time...";
+  Directions? _directions;
 
   @override
   Widget build(BuildContext context) {
-    List _arguments = ModalRoute.of(context)!.settings.arguments as List;
-    Position _position = _arguments[0];
-    PlaceApi.getPlace(_position).then((place) => setState(() { _positionName = place.name; }));
-    Place _search = _arguments[1];
+    if (_position == null) {
+      List _arguments = ModalRoute.of(context)!.settings.arguments as List;
+
+      Position origin = _arguments[0];
+      Place destination = _arguments[1];
+
+      setState(() {
+        _destination = _arguments[1];
+        _destinationName = _arguments[1].name;
+        _position = _arguments[0];
+      });
+
+      PlaceApi.getPlace(_position).then((place) => setState(() {
+            _positionName = place.name;
+          }));
+
+      DirectionsAPI()
+          .getDirections(
+              origin: LatLng(origin.latitude, origin.longitude),
+              destination: destination)
+          .then((directions) {
+        setState(() {
+          _directions = directions;
+          _estimatedTime = directions!.totalDuration;
+        });
+      });
+    }
+
     return Scaffold(
       body: PageBackground(
         child: Column(
           children: [
             Align(alignment: Alignment.topLeft, child: BackButton()),
             Expanded(
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: FittedBox(
-                  fit: BoxFit.contain,
-                  child: Card(
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10)
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.all(24.0),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          Text('Destination:',
-                            style: kTitleStyle.copyWith(fontSize: 32),),
-                          Text(_search.name,
-                            style: kTitleStyle.copyWith(fontSize: 32),
-                            softWrap: true,),
-                          SizedBox(height: 25),
-                          // TODO: Get the estimated travel time
-                          Text('Estimated Time: '
-                              + getEstimatedTime().toString()
-                              + ' minutes',
-                            style: kSubTitleStyle.copyWith(fontSize: 28),
-                            textAlign: TextAlign.start, softWrap: true,),
-                          // TODO: Get the current location as address
-                          Text('Starting From: ' + _positionName,
-                            style: kSubTitleStyle.copyWith(fontSize: 28),
-                            textAlign: TextAlign.start, softWrap: true,),
-                          SizedBox(height: 25),
-                          TextButton(onPressed: () {
-                            Navigator.pushNamed(context, NavigationPage.id, arguments: [_position, _positionName, _search]);
-                          }, style: kRedButtonStyle,
-                            child: Padding(
+                child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: FittedBox(
+                fit: BoxFit.contain,
+                child: Card(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                  child: Padding(
+                    padding: const EdgeInsets.all(24.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Text(
+                          'Destination:',
+                          style: kTitleStyle.copyWith(fontSize: 32),
+                        ),
+                        Text(
+                          _destinationName,
+                          style: kTitleStyle.copyWith(fontSize: 32),
+                          softWrap: true,
+                        ),
+                        SizedBox(height: 25),
+                        Text(
+                          'Estimated Time: ' + _estimatedTime,
+                          style: kSubTitleStyle.copyWith(fontSize: 28),
+                          textAlign: TextAlign.start,
+                          softWrap: true,
+                        ),
+                        Text(
+                          'Starting From: ' + _positionName,
+                          style: kSubTitleStyle.copyWith(fontSize: 28),
+                          textAlign: TextAlign.start,
+                          softWrap: true,
+                        ),
+                        SizedBox(height: 25),
+                        TextButton(
+                          onPressed: () {
+                            Navigator.pushNamed(context, NavigationPage.id,
+                                arguments: [
+                                  _position,
+                                  _positionName,
+                                  _destination,
+                                  _directions
+                                ]);
+                          },
+                          style: kRedButtonStyle,
+                          child: Padding(
                               padding: const EdgeInsets.all(8.0),
-                                child: Text('Start Navigation',
-                                    style:
-                                    kTitleStyle.copyWith(color: Colors.white,
-                                        fontSize: 32)
-                                )
-                            ),
-                          )
-                        ],
-                      ),
+                              child: Text('Start Navigation',
+                                  style: kTitleStyle.copyWith(
+                                      color: Colors.white, fontSize: 32))),
+                        )
+                      ],
                     ),
                   ),
                 ),
-              )
-              
-            ),
+              ),
+            )),
           ],
         ),
       ),
     );
-  }
-
-  // TODO: use the Google API to get an estimated travel time
-  // TODO: replace dest with chosen destination's lat/long
-  // issue: not actually getting the positions, only going to the second return
-  int getEstimatedTime() {
-    LocationApi.getCurrentLocation().then((Position location) {
-      var og = LatLng(location.latitude, location.longitude);
-      var dest = PlaceApi.getPlace(LatLng(og.latitude, og.longitude));
-      dest.then((Place p) {
-        Future<Directions?> dir = DirectionsAPI().getDirections(
-            origin: og, destination: p);
-        dir.then((Directions? d) {
-          return d!.totalDuration;
-        });
-      });
-    });
-    return -1;
   }
 }
