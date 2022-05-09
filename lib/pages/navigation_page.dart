a0c73d44ee26a64a67ec6226acf26ab613973918
emilychang9 <69648669+emilychang9@users.noreply.github.com>
2022-05-08 20:38:58 -0700

    Fixing typo that prevented successful compile

--- lib/pages/navigation_page.dart
+++ lib/pages/navigation_page.dart
@@ -29,18 +29,18 @@ class _NavigationPageState extends State<NavigationPage> {
   }
 
   get endNavigationButton => TextButton(
-        child: const Text(
-          'End Navigation',
-          style: kTitleStyle,
-        ),
-        onPressed: () {
-          LocationApi.stopLocationUpdates();
-          LocationApi.removeOnLocationUpdateListener(onLocationUpdated);
-          Navigator.popUntil(context, ModalRoute.withName('search_page'));
-          SemanticsService.announce("Ending navigation", TextDirection.ltr);
-        },
-        style: kRedButtonStyle,
-      );
+    child: const Text(
+      'End Navigation',
+      style: kTitleStyle,
+    ),
+    onPressed: () {
+      LocationApi.stopLocationUpdates();
+      LocationApi.removeOnLocationUpdateListener(onLocationUpdated);
+      Navigator.popUntil(context, ModalRoute.withName('search_page'));
+      SemanticsService.announce("Ending navigation", TextDirection.ltr);
+    },
+    style: kRedButtonStyle,
+  );
 
   @override
   Widget build(BuildContext context) {
@@ -53,73 +53,72 @@ class _NavigationPageState extends State<NavigationPage> {
     }
 
     return Scaffold(
-        body: PageBackground(
-            child: Column(
-      children: [
-        Card(
-          margin: EdgeInsets.symmetric(horizontal: 8.0, vertical: 24.0),
-          shadowColor: Colors.blueGrey,
-          child: MergeSemantics(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Container(
-                      width: DEVICE_WIDTH,
-                      child: Text("NOW: Prepare to turn right onto NE 45th St",
-                          style: TextStyle(
-                              fontSize: 25,
-                              fontWeight: FontWeight.w700,
-                              color: Colors.black),
-                          textAlign: TextAlign.center)),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                  child: Column(
+      body: PageBackground(
+        child: Column(
+          children: [
+            Card(
+              margin: EdgeInsets.symmetric(horizontal: 8.0, vertical: 24.0),
+              shadowColor: Colors.blueGrey,
+              child: MergeSemantics(
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Container(
+                Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                    width: DEVICE_WIDTH,
+                    child: Text("NOW: Prepare to turn right onto NE 45th St",
+                        style: TextStyle(
+                            fontSize: 25,
+                            fontWeight: FontWeight.w700,
+                            color: Colors.black),
+                        textAlign: TextAlign.center)),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: Column(
+                  children: [
+                    Container(
+                        width: DEVICE_WIDTH,
+                        child: Text("To: " + _destination,
+                            style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.w500,
+                                color: Colors.black),
+                            textAlign: TextAlign.center)),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
                           width: DEVICE_WIDTH,
-                          child: Text("To: " + _destination,
+                          child: Text("Current Location: " + _currentPositionName,
                               style: TextStyle(
-                                  fontSize: 20,
+                                  fontSize: 18,
                                   fontWeight: FontWeight.w500,
                                   color: Colors.black),
                               textAlign: TextAlign.center)),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Container(
-                            width: DEVICE_WIDTH,
-                            child: Text("Current Location: " + _currentPositionName,
-                                style: TextStyle(
-                                    fontSize: 18,
-                                    fontWeight: FontWeight.w500,
-                                    color: Colors.black),
-                                textAlign: TextAlign.center)),
-                      ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
               ),
-            ],
-          ),
+          ],),
         ),
-        endNavigationButton,
-        Semantics(
-          child: const Image(
+      ),
+      endNavigationButton,
+      Semantics(
+        child: const Image(
             image: AssetImage('images/hapticTouchButton.png'),
             height: 250,
             alignment: Alignment.bottomCenter),
-          label: 'Keep finger on the screen for haptic feedback',
-        ),
+        label: 'Keep finger on the screen for haptic feedback',
+      ),
       ],
     )));
   }
 
   onLocationUpdated(pos) {
     PlaceApi.getPlace(pos).then((place) => setState(() {
-          _currentPosition = pos;
-          _currentPositionName = place.name;
-        }));
+      _currentPosition = pos;
+      _currentPositionName = place.name;
+    }));
   }
 }
