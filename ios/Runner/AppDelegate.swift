3270273ddae657df0ad91c1ddd4ace13986e6b31
pulkitm1999 <malhop@uw.edu>
2022-05-09 15:00:10 -0700

    some random fixes

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -10,7 +10,7 @@ import GoogleMaps
     GeneratedPluginRegistrant.register(with: self)
 
     // TODO: Add your Google Maps API key
-    GMSServices.provideAPIKey("AIzaSyCCIfDcZEOCFAOyDiuKEkU2UimbJgUORNI")
+    GMSServices.provideAPIKey("")
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
