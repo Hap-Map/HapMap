dc05a7713cf61d16370ebeca9269f8da20fd42eb
Aditya Nayak <nayakadityam@gmail.com>
2022-05-09 13:04:10 -0700

    Added import statement to fix bug

--- lib/pages/confirm_page.dart
+++ lib/pages/confirm_page.dart
@@ -7,6 +7,7 @@ import 'package:hap_map/pages/navigation_page.dart';
 import '../api/directions_api.dart';
 import '../api/location_api.dart';
 import '../api/place_api.dart';
+import '../main.dart';
 import '../models/directions_model.dart';
 import '../models/place_model.dart';
 
