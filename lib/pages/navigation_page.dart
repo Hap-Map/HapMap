2b8c60d2038f86299bce2f3076f4cf65bbfe5b9c
pulkitm1999 <malhop@uw.edu>
2022-05-09 13:39:32 -0700

    some functionality

--- lib/pages/navigation_page.dart
+++ lib/pages/navigation_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter_html/flutter_html.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter_platform_interface/src/types/location.dart';
 import 'package:hap_map/constants.dart';
 import 'package:hap_map/main.dart';
 import 'package:hap_map/models/directions_model.dart';
@@ -19,12 +20,16 @@ class NavigationPage extends StatefulWidget {
   State<NavigationPage> createState() => _NavigationPageState();
 }
 
+double METERS_TO_UPDATE_PLACE = 100;
+double METERS_EPSILON = 10;
+
 class _NavigationPageState extends State<NavigationPage> {
   Position? _currentPosition;
   Place? _current;
   Place? _destination;
   Directions? _directions;
   DirectionsIterator? _iter;
+  bool _destination_reached = false;
 
   _NavigationPageState() {
     LocationApi.addOnLocationUpdateListener(onLocationUpdated);
@@ -80,7 +85,7 @@ class _NavigationPageState extends State<NavigationPage> {
                 padding: const EdgeInsets.all(8.0),
                 child: Container(
                     width: DEVICE_WIDTH,
-                    child: Html(data: _iter!.getStepStr(),
+                    child: Html(data: _destination_reached? '<html><body></b>Destination Reached</b></body></html>' : _iter!.getStepStr(),
                         style: {
                             "body" : Style(
                             fontSize: FontSize(25),
@@ -130,12 +135,39 @@ class _NavigationPageState extends State<NavigationPage> {
     )));
   }
 
-  onLocationUpdated(pos) {
-    PlaceApi.getPlace(pos).then((place) => setState(() {
-         _currentPosition = pos;
-          _current = place;
-          // TODO: CHECK IF END OF STEP IS REACHED AND UPDATE
-        }));
+  onLocationUpdated(Position pos) {
+    Position _old = _currentPosition!;
+    setState(() {
+      _currentPosition = pos;
+    });
+    if (isFarEnough(_old, pos)) {
+      PlaceApi.getPlace(pos).then((place) =>
+          setState(() {
+            _currentPosition = pos;
+            _current = place;
+          }));
+    } else if (isCloseEnough(_iter!.getStepEnd()!, pos)) {
+      if (_iter!.hasNext()) {
+        _iter!.moveNext();
+      } else {
+        _destination_reached = true;
+      }
+    }
+  }
+
+  bool isFarEnough(Position p1, Position p2) {
+    return distance(p1, p2) > METERS_TO_UPDATE_PLACE;
+  }
+
+  double distance(Position p1, Position p2) {
+    return Geolocator.distanceBetween(p1.latitude, p1.longitude, p2.latitude, p2.longitude);
+  }
+  double distanceLatLng(LatLng p1, Position p2) {
+    return Geolocator.distanceBetween(p1.latitude, p1.longitude, p2.latitude, p2.longitude);
+  }
+
+  bool isCloseEnough(LatLng p1, Position p2) {
+    return distanceLatLng(p1, p2) < METERS_EPSILON;
   }
 
 }
