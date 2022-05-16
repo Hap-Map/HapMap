80b0bf372bfbfda07c72a04beeb975a8adf7c784
emilychang9 <emilycc2000@gmail.com>
2022-05-16 15:10:41 -0700

    Added white background for better contrast.

--- lib/pages/help_page.dart
+++ lib/pages/help_page.dart
@@ -49,27 +49,43 @@ class _HelpPage extends State<HelpPage> {
           child: Column(
             children: [
               const Align(alignment: Alignment.topLeft, child: BackButton()),
-              const Text(
-                "How to Use HapMap:",
-                style: kTitleStyle,
-              ),
-              const SizedBox(height: 25),
               Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 24.0),
-                child:  Align(
-                  alignment: Alignment.centerLeft,
-                  child: Column(
-                    children: [for(var i in _instructions)
-                      Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 5),
-                        child: Text(i,
-                          style: kBodyStyle,),
-                      )
-                    ],
+                  padding: EdgeInsets.all(16.0),
+                child: Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      children: [
+                        const SizedBox(height: 12),
+                        const Text(
+                          "How to Use HapMap:",
+                          style: kTitleStyle,
+                        ),
+                        const SizedBox(height: 12),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 24.0),
+                          child:  Align(
+                            alignment: Alignment.centerLeft,
+                            child: Column(
+                              children: [for(var i in _instructions)
+                                Padding(
+                                  padding: const EdgeInsets.symmetric(vertical: 5),
+                                  child: Text(i,
+                                    style: kBodyStyle,),
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ),
-              const SizedBox(height: 25),
+              const SizedBox(height: 12),
               TextButton(
                 onPressed: (){},
                 style: kBlueButtonStyle,
@@ -82,8 +98,8 @@ class _HelpPage extends State<HelpPage> {
                 padding: EdgeInsets.all(24.0),
                 child: Text("For any issues with this app, "
                     "submit your feedback on our official "
-                    "Github Repository's issues tab here: "
-                    "https://github.com/Hap-Map/HapMap",
+                    "Github Repository's issues tab: "
+                    "https://github.com/Hap-Map/HapMap/issues",
                   textAlign: TextAlign.center,),
               ),
             ],
