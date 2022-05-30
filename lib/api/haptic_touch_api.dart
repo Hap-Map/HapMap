655a6da081703311e3d02d39617ce582fb849d5d
Aditya Nayak <nayakadityam@gmail.com>
2022-05-30 15:49:14 -0700

    Added various feedback options

--- lib/api/haptic_touch_api.dart
+++ lib/api/haptic_touch_api.dart
@@ -5,12 +5,30 @@ class HapticFeedbackApi {
     Vibrate.feedback(type);
   }
 
-  static generateFeedbackSequence(List feedbackTypes, List intervalDurationsMillis, {int repetitions = 1}) {
+  static generateFeedbackSequence(List<FeedbackType> feedbackTypes, List<int> intervalDurationsMillis, {int repetitions = 1}) {
     assert (feedbackTypes.length == intervalDurationsMillis.length);
 
     for (int i = 0; i < feedbackTypes.length; i++) {
       generateFeedback(feedbackTypes[i]);
       Future.delayed(Duration(milliseconds: intervalDurationsMillis[i]));
     }
+  }
+
+  static generateLeftFeedback() {
+    List<FeedbackType> feedbackTypes = [FeedbackType.selection, FeedbackType.impact];
+    List<int> intervalDurationsMillis = [250, 750];
+    generateFeedbackSequence(feedbackTypes, intervalDurationsMillis, repetitions: 5);
+  }
+
+  static generateRightFeedback() {
+    List<FeedbackType> feedbackTypes = [FeedbackType.warning, FeedbackType.heavy];
+    List<int> intervalDurationsMillis = [250, 750];
+    generateFeedbackSequence(feedbackTypes, intervalDurationsMillis, repetitions: 5);
+  }
+
+  static generateDestinationFeedback() {
+    List<FeedbackType> feedbackTypes = [FeedbackType.success];
+    List<int> intervalDurationsMillis = [250];
+    generateFeedbackSequence(feedbackTypes, intervalDurationsMillis, repetitions: 10);
   }
 }
