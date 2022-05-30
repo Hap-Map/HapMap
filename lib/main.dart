79e114821cad8d083821b4c106afcf9ed3548737
pulkitm1999 <malhop@uw.edu>
2022-05-30 14:23:00 -0700

    added a first run check in main()

--- lib/main.dart
+++ lib/main.dart
@@ -4,19 +4,26 @@ import 'package:hap_map/pages/help_page.dart';
 import 'package:hap_map/pages/navigation_page.dart';
 import 'package:hap_map/pages/search_page.dart';
 import 'package:hap_map/pages/settings_page.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
-  runApp(const HapMap());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  bool first = !prefs.containsKey('first_run');
+  prefs.setBool('first_run', false);
+  runApp(HapMap(firstRun: first));
 }
 
 class HapMap extends StatelessWidget {
-  const HapMap({Key? key}) : super(key: key);
+  final bool firstRun;
+  HapMap({Key? key, this.firstRun = false}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       key: const Key('HapMap'),
-      initialRoute: SearchPage.id,
+      // TODO: Add TutorialPage here vvv
+      initialRoute: firstRun? HelpPage.id : SearchPage.id,
       routes: {
         SearchPage.id: (context) => const SearchPage(),
         HelpPage.id: (context) => const HelpPage(),
